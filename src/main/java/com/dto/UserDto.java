bff7acce3e8f45d4abb6e49745fad84cec4d1fdd
1040230345 <1040230345@qq.com>
2019-08-16 14:49:35 +0800

    jackson

--- src/main/java/com/dto/UserDto.java
+++ src/main/java/com/dto/UserDto.java
@@ -1,4 +1,83 @@
 package com.dto;
 
+import java.util.Date;
+
+/**
+ * 时间：2019年8月16日14:21:16
+ */
 public class UserDto {
+
+    private Long id;  //用户id，唯一
+    private String email;  // 用户邮箱，唯一
+    private String name;  //用户名字，唯一
+    private String user_password; //密码
+    private String avatar_url;  //用户头像链接，有默认
+    private String bio;  //用户简介
+    private Date created_at; //创建时间
+    private Date updated_at; //修改时间
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser_password() {
+        return user_password;
+    }
+
+    public void setUser_password(String user_password) {
+        this.user_password = user_password;
+    }
+
+    public String getAvatar_url() {
+        return avatar_url;
+    }
+
+    public void setAvatar_url(String avatar_url) {
+        this.avatar_url = avatar_url;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
 }
