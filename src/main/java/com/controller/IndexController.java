5b43bc91d22b339c292214df3316b781e17e7988
1040230345 <1040230345@qq.com>
2019-08-18 00:47:56 +0800

    退出功能

--- src/main/java/com/controller/IndexController.java
+++ src/main/java/com/controller/IndexController.java
@@ -3,6 +3,10 @@ package com.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * 主页控制器
  * 作者：jackson
@@ -13,6 +17,23 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index(){
+        return "index";
+    }
+
+    @GetMapping("/loginout")
+    public String login_out(HttpServletRequest request, HttpServletResponse response){
+        Cookie[] cookies = request.getCookies();
+
+        if(cookies!=null){
+            for(Cookie cookie:cookies){
+                if(cookie.getName().equals("TOKEN")){
+                    cookie.setValue(null);
+                    cookie.setMaxAge(0);
+                    response.addCookie(cookie);
+                }
+            }
+        }
+
         return "index";
     }
 
