4ea34ad9937d4deb4557ecbfbac800f60b208aa0
1040230345 <1040230345@qq.com>
2019-08-22 22:47:29 +0800

    修复了登录注册的拦截问题

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -21,42 +20,10 @@ public class HomeController {
     private UserService userService;
 
     @GetMapping("/home")
-    public String home(HttpServletRequest request,Model model){
-        //优先判断是否有session存在
+    public String home(HttpServletRequest request,Model model) {
         HttpSession session = request.getSession();
-        if(session.getAttribute("User_id")!=null){
-            //依靠user_id返回用户资料
-            UserDto userDto = userService.findUserByid((int)session.getAttribute("User_id"));
-            if(userDto.getAvatar_url()==null){
-                userDto.setAvatar_url("/images/superengineer.jpg");
-            }
-            model.addAttribute("USER",userDto);
-            return "home";
-        }
-        //检查home页面是否有cookie
-        Cookie[] cookies = request.getCookies();
-        //防止空指针异常
-        if(cookies!=null){
-            for(Cookie cookie:cookies){
-                //假如用户的状态还是登陆着的
-                if(cookie.getName().equals("TOKEN")){
-                    //依靠cookie查找用户资料，返回前端
-                    int user_id = userService.findUserIdByCookie(cookie.getValue());
-                    //如果找不到这个用户
-                    if(user_id==0){
-                        return "index";
-                    }
-                    //依靠user_id返回用户资料
-                    UserDto userDto = userService.findUserByid(user_id);
-                    if(userDto.getAvatar_url()==null){
-                        userDto.setAvatar_url("/images/superengineer.jpg");
-                    }
-                    model.addAttribute("USER",userDto);
-                    return "home";
-                }
-            }
-        }
-
-        return "index";
+        UserDto userDto = (UserDto) session.getAttribute("User");
+        model.addAttribute("USER",userDto);
+        return "/home";
     }
 }
--- src/main/java/com/interceptor/SessionInterceptor.java
+++ src/main/java/com/interceptor/SessionInterceptor.java
@@ -1,4 +1,7 @@
 package com.interceptor;
+import com.dto.UserDto;
+import com.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,6 +13,9 @@ import javax.servlet.http.HttpSession;
 @Service
 public class SessionInterceptor implements HandlerInterceptor {
 
+    @Autowired
+    private UserService userService;
+
     /**
      * 对除了主页外的所有页面进行拦截验证
      * @param request
@@ -18,30 +24,49 @@ public class SessionInterceptor implements HandlerInterceptor {
      * @return
      * @throws Exception
      */
-    @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //优先判断是否有session存在
         HttpSession session = request.getSession();
-        if(session.getAttribute("User_id")!=null){
+        if (session.getAttribute("User_id") != null) {
+            //依靠user_id返回用户资料
+            UserDto userDto = userService.findUserByid((int) session.getAttribute("User_id"));
+            if (userDto.getAvatar_url() == null) {
+                userDto.setAvatar_url("/static/images/superengineer.jpg");
+            }
+            session.setAttribute("User",userDto);
             //如果session存在，放行
             return true;
-        }else {
+        } else {
             //判断cookie是否存在
             Cookie[] cookies = request.getCookies();
             //防止空指针异常
-            if(cookies!=null){
-                for(Cookie cookie:cookies){
+            if (cookies != null) {
+                for (Cookie cookie : cookies) {
                     //假如用户的状态还是登陆着的
-                    if(cookie.getName().equals("TOKEN")){
+                    if (cookie.getName().equals("TOKEN")) {
+                        //依靠cookie查找用户资料，返回前端
+                        int user_id = userService.findUserIdByCookie(cookie.getValue());
+                        //如果找不到这个用户
+                        if (user_id == 0) {
+                            //页面跳转
+                            response.sendRedirect("/");
+                            return false;
+                        }
+                        //依靠user_id返回用户资料
+                        UserDto userDto = userService.findUserByid(user_id);
+                        if (userDto.getAvatar_url() == null) {
+                            userDto.setAvatar_url("/static/images/superengineer.jpg");
+                        }
+                        session.setAttribute("User",userDto);
                         //放行
                         return true;
-                        }
+                    }
                 }
             }
+            //页面跳转
+            response.sendRedirect("/");
+            return false;
         }
-        //页面跳转
-        response.sendRedirect("/");
-        return false;
     }
 
     @Override
