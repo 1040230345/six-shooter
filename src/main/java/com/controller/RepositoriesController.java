e8b10acde1cf7c89e44ae52a5e8b3734ecc34a07
1040230345 <1040230345@qq.com>
2019-08-23 14:42:53 +0800

    jackson

--- src/main/java/com/controller/RepositoriesController.java
+++ src/main/java/com/controller/RepositoriesController.java
@@ -1,7 +1,14 @@
 package com.controller;
 
+import com.dto.UserDto;
+import com.mapper.MailMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sun.plugin2.main.client.MozillaServiceDelegate;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * 仓库控制器
@@ -11,7 +18,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class RepositoriesController {
 
     @GetMapping("/newRepo")
-    public String newRepo(){
+    public String newRepo(HttpServletRequest request, Model model){
+        //获取session内容返回用户信息
+        HttpSession session = request.getSession();
+        UserDto userDto = (UserDto) session.getAttribute("User");
+        model.addAttribute("USER",userDto);
         return "/newRepo";
     }
 
