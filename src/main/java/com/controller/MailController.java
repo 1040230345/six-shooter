642dabb28744f142f17c56a41b833f5b16c1711d
1040230345 <1040230345@qq.com>
2019-10-03 12:22:47 +0800

    临时维护，暂不能用

--- src/main/java/com/controller/MailController.java
+++ src/main/java/com/controller/MailController.java
@@ -6,15 +6,17 @@ import com.my_util.GetTime_util;
 import com.service.MailService;
 import com.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 @Controller
 public class MailController {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
     @Autowired
     private MailService mailService;
     @Autowired
@@ -27,53 +29,61 @@ public class MailController {
     @PostMapping("/vcodeSend")
     @ResponseBody
     public Map<String,String> getCheckCode(String mail){
+//        Map<String,String> map = new HashMap<>();
+//        //优先判断是用户名还是邮箱
+//        boolean ifmail = mail.contains("@");
+//        //如果不是邮箱格式
+//        if(!ifmail){
+//            //那就依靠用户名查找邮箱咯
+//            mail = userService.findEmailByName(mail);
+//            if(mail == null){
+//                //找不到对应的邮箱直接返回0
+//                map.put("email", "0");
+//                return map;
+//            }
+//        }
+//        //生成验证码
+//        String checkCode = String.valueOf(new Random().nextInt(899999) + 100000);
+//        String message = "您的注册验证码为："+checkCode;
+//        //先判断数据库里面是否已经有信息
+//        if( mailMapper.findCodeByEmail(mail)!=null) {
+//            //删除数据库信息
+//            int mun = mailMapper.delCodeByEmail(mail);
+//            if(mun ==0 ){
+//                //删除异常
+//                map.put("email", "0");
+//                return map;
+//            }
+//        }
+//        try {
+//            //发送邮件
+//            mailService.sendSimpleMail(mail, "注册验证码", message);
+//            //写入数据库
+//            CodeDto codeDto = new CodeDto();
+//            codeDto.setEmail(mail);
+//            codeDto.setCode(checkCode);
+//            codeDto.setCreated_at(getTime_util.GetNowTime_util());
+//            int num = mailMapper.insertCookie(codeDto);
+//            if(num>1){
+//                //写入成功
+//                map.put("email", "1");
+//                return map;
+//            }else {
+//                //失败
+//                map.put("email", "0");
+//                return map;
+//            }
+//        }catch (Exception e){ }
+//        map.put("email", "0");
         Map<String,String> map = new HashMap<>();
-        //优先判断是用户名还是邮箱
-        boolean ifmail = mail.contains("@");
-        //如果不是邮箱格式
-        if(!ifmail){
-            //那就依靠用户名查找邮箱咯
-            mail = userService.findEmailByName(mail);
-            if(mail == null){
-                //找不到对应的邮箱直接返回0
-                map.put("email", "0");
-                return map;
-            }
-        }
-        //生成验证码
-        String checkCode = String.valueOf(new Random().nextInt(899999) + 100000);
-        String message = "您的注册验证码为："+checkCode;
-        //先判断数据库里面是否已经有信息
-        if( mailMapper.findCodeByEmail(mail)!=null) {
-            //删除数据库信息
-            int mun = mailMapper.delCodeByEmail(mail);
-            if(mun ==0 ){
-                //删除异常
-                map.put("email", "0");
-                return map;
-            }
-        }
         try {
-            //发送邮件
-            mailService.sendSimpleMail(mail, "注册验证码", message);
-            //写入数据库
-            CodeDto codeDto = new CodeDto();
-            codeDto.setEmail(mail);
-            codeDto.setCode(checkCode);
-            codeDto.setCreated_at(getTime_util.GetNowTime_util());
-            int num = mailMapper.insertCookie(codeDto);
-            if(num>1){
-                //写入成功
-                map.put("email", "1");
-                return map;
-            }else {
-                //失败
-                map.put("email", "0");
-                return map;
-            }
-        }catch (Exception e){ }
-        map.put("email", "0");
-        return map;
+            mailService.getCheckCode(mail);
+            map.put("email", "1");
+            return map;
+        }catch (Exception e){
+            map.put("email", "0");
+            return map;
+        }
     }
 
 }
--- src/main/java/com/service/MailService.java
+++ src/main/java/com/service/MailService.java
@@ -2,10 +2,14 @@ package com.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 @Service("mailService")
 @Transactional
@@ -14,9 +18,11 @@ public class MailService {
     private String from;
     @Autowired
     private JavaMailSender mailSender;
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
     //发送邮箱
-    public void sendSimpleMail(String to,String title,String content){
+    public void sendSimpleMail(String to, String title, String content) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setFrom(from);
         message.setTo(to);
@@ -26,4 +32,22 @@ public class MailService {
 
     }
 
+    //获取邮箱验证码
+    public boolean getCheckCode(String mail) {
+        //先检查缓存中是否存在值
+        boolean bl = stringRedisTemplate.hasKey(mail);
+        if (bl) {
+            //删除值
+            stringRedisTemplate.delete(mail);
+        }
+        //生成验证码
+        String checkCode = String.valueOf(new Random().nextInt(899999) + 100000);
+        String message = "您的注册验证码为：" + checkCode;
+        //发送邮件
+        sendSimpleMail(mail, "注册验证码", message);
+        System.out.println(mail);
+        //存入缓存
+        stringRedisTemplate.opsForValue().set(mail, checkCode, 60 * 2, TimeUnit.SECONDS);//向redis里存入数据和设置缓存时间
+        return true;
+    }
 }
